aecf3c49dd9c59436d2f8d613cc96f29e559ac6b
Rohith k <rohith.ktpb333@gmail.com>
2025-08-06 18:01:30 +0530

    chnages

--- android/app/src/main/kotlin/dev/repad/cluster/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.repad.cluster
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/dev/repad/cluster/just_checking/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.repad.cluster.just_checking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
