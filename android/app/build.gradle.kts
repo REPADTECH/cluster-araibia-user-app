b8f034b9eb6aee05c22257c8fc888c0e15a3bfb0
Rohith k <rohith.ktpb333@gmail.com>
2025-10-09 11:30:27 +0530

    chnages

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
     }
 
     buildTypes {
